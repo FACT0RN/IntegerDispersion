79d06a8eab2a4d58dd7d54a96ae5ad18ad3373de
Escanor Liones <ReverseControl@users.noreply.github.com>
2023-04-29 08:45:15 -0400

    [renice_mining.py] Used to increase miners' priorities for the Linux scheduler: reduces context switches and core migrations, increases time slices on the scheduler.

---
+++ renice_mining.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def get_yafu_pids():
+
+    run_command = "ps -a"
+    parse = subprocess.run( run_command, capture_output=True, shell=True, timeout = 10 )
+    parse = parse.stdout.decode('utf-8').split("\n")
+    parse = [ line.split()[0] for line in parse if "yafu" in line ]
+    for idx, line in enumerate( parse ):
+        print( idx + 1, line)
+
+    return parse
+
+def renice_pids( pids = []):
+
+    for pid in pids:
+        run_command = "sudo renice -15 -p " + pid
+        parse = subprocess.run( run_command, capture_output=True, shell=True, timeout = 10 )
+        parse = parse.stdout.decode('utf-8').split("\n")
+
+pids = get_yafu_pids()
+renice_pids( pids )
